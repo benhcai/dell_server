9a5a95220ffa6b540249166ad4d5aa365c7e03ef
lunrsai <lunrsai@gmail.com>
2023-12-30 23:24:42 +0800

    pass

--- commands.js
+++ commands.js
@@ -5,4 +5,7 @@ module.exports = [
   " SSHPASS=$DELLPASS sshpass -e ssh root@192.168.2.120 'racadm serveraction graceshutdown'",
   " SSHPASS=$DELLPASS sshpass -e ssh root@192.168.2.120 'racadm get system.Power.Realtime.Power'",
   " SSHPASS=$ESXIPASS sshpass -e ssh root@192.168.2.130 'vim-cmd vmsvc/getallvms'",
+  " SSHPASS=$ESXIPASS sshpass -e ssh root@192.168.2.130 'vim-cmd vmsvc/power.getstate 1'",
+  " SSHPASS=$ESXIPASS sshpass -e ssh root@192.168.2.130 'vim-cmd vmsvc/power.on 1'",
+  " SSHPASS=$ESXIPASS sshpass -e ssh root@192.168.2.130 'vim-cmd vmsvc/power.off 1'",
 ];
